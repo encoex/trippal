0b52a22514d5b4a2d869f3466e48e1f4263a6ab2
Yigit Can Arin <yigitcan@microsoft.com>
2014-03-12 23:00:00 +0100

    starting to add viewModels

---
+++ PhotoBuddy/PhotoDisplayApp/ViewModels/Album.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoDisplayApp.ViewModels
+{
+    public class Album
+    {
+        IEnumerable<Photo> photos;
+        public int AlbumId { get; set; }
+        public string AlbumName { get; set; }        
+        public void GetPhotosAsync() { }
+        public void AddPhotoAsync() { }
+        public void DeletePhotoAsync() { }
+    }
+}
---
+++ PhotoBuddy/PhotoDisplayApp/ViewModels/AlbumsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoDisplayApp.ViewModels
+{
+    public class AlbumsViewModel
+    {
+        private IEnumerable<Album> albums;
+
+        public void CreateAlbumAsync(){ }
+        public void DeleteAlbumAsync() { }
+      
+    }
+}
